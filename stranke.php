1054b2933114bff9b156fc3239d61ffbfbb2337f
nyxthejon <jon.r.gorsek@gmail.com>
2022-07-27 15:10:51 +0200

    priprava izpisa za stranke

--- stranke.php
+++ stranke.php
@@ -4,10 +4,54 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="stranke.css">
     <title>Document</title>
 </head>
 <body>
     <?php require 'header.php' ?> 
     Stranke :)
+
+    <div class="container">
+    <div class="container-fluid p-0">
+
+		<div class="row">
+			<div class="col-xl-8">
+				<div class="card">
+					<div class="card-header pb-0">
+						<div class="card-actions float-right">
+						</div>
+					</div>
+					<div class="card-body">
+						<table class="table table-striped" style="width:100%">
+							<thead>
+								<tr>
+									<th>Ime</th>
+									<th>Email</th>
+									<th>Telefon</th>
+                                    <th>Ogled</th>
+								</tr>
+							</thead>
+							<tbody>
+								<tr>
+									<td>Jon rg</td>
+									<td>jon@winters.com</td>
+                                    <td>12313124</td>
+									<td>GUMB</td>
+								</tr>
+								<tr>
+                                    <td>Jon gr</td>
+									<td>omg@safsa</td>
+									<td>12312321</td>
+									<td>GUMB</td>
+								</tr>
+							
+							</tbody>
+						</table>
+					</div>
+				</div>
+			</div>
+
+	</div>
+</div>
 </body>
 </html>
